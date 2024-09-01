7c3f4a9e5bbc191d55a70d17ba15ffa701b1a70e
Jasmineee <163031279+JasmineeeWu@users.noreply.github.com>
2024-09-01 17:33:33 +0100

    Delete DQN.py

--- DQN.py
+++
@@ -1,8 +0,0 @@
-#Define the DQN Model with TensorFlow
-def build_dqn_model(input_shape, output_shape):
-    model = models.Sequential()
-    model.add(layers.Dense(64, input_shape=input_shape, activation='relu'))
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(output_shape, activation='linear'))
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss='mse')
-    return model
