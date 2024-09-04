881bedddd092acee4605656bd47ac3338956b4d3
Jasmineee <163031279+JasmineeeWu@users.noreply.github.com>
2024-09-04 19:49:03 +0100

    Update DQN model.py

--- DQN model.py
+++ DQN model.py
@@ -62,8 +62,8 @@ class OptionHedgingEnv(gym.Env):
 # Define the DQN Model
 def build_dqn_model(input_shape, output_shape):
     model = models.Sequential()
-    model.add(layers.Dense(16, input_shape=input_shape, activation='relu'))
-    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense(64, input_shape=input_shape, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(output_shape, activation='linear'))
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss='mse')
     return model
